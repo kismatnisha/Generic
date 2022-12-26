4332f048f93000b41887012449e878d43e66f841
qismat <qismatnisha6243@gmail.com>
2022-12-26 16:49:10 +0530

    [qismat] Add : UC1-TestCases

---
+++ FindMaxNumber/FindMaxNumber/MaximumNumber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNumber
+{
+    public class MaximumNumber
+    {
+        public static int MaximumNum(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+
+        public static Double MaximumNum(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+        public static string MaximumNum(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+    }
+}
--- FindMaxNumber/FindMaxNumber/Program.cs
+++ FindMaxNumber/FindMaxNumber/Program.cs
@@ -4,9 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("maximum no is");
-            int output = MaximumIntNumber.MaximumIntNum(11, 25, 33);
+            Console.WriteLine("maximum INT no is");
+            int output = MaximumNumber.MaximumNum(11, 25, 33);
             Console.WriteLine(output);
+
+            Console.WriteLine("Maximum Double no is");
+            double output1 = MaximumNumber.MaximumNum(11.5, 22.5, 33.5);
+            Console.WriteLine(output1);
+
+            Console.WriteLine("Maximum String no is");
+            string output2 = MaximumNumber.MaximumNum("abc", "xyz", "pqr");
+            Console.WriteLine(output1);
         }
+
     }
 }
