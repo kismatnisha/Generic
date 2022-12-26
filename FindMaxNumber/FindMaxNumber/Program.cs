e883e84cd3608b252deeef8186487ebd0adc887b
qismat <qismatnisha6243@gmail.com>
2022-12-26 16:40:37 +0530

    [qismat] Add : UC1-MaximumOfThreeInt

---
+++ FindMaxNumber/FindMaxNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("maximum no is");
+            int output = MaximumIntNumber.MaximumIntNum(11, 25, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
