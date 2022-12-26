14fbf0d11da57dff41dc6d09809aaeed4c7a68e1
qismat <qismatnisha6243@gmail.com>
2022-12-26 17:14:20 +0530

    [qismat] Add : UC3-Refactor-GenericClass

--- FindMaxNumber/FindMaxNumber/MaximumNumber.cs
+++ FindMaxNumber/FindMaxNumber/MaximumNumber.cs
@@ -6,26 +6,38 @@ using System.Threading.Tasks;
 
 namespace FindMaxNumber
 {
-    public class MaximumNumber
-    {
-        public static T GetMaximum<T>(T first_Value, T second_Value, T third_Value) where T : IComparable<T>
+   
+        public class MaximumNumber<T> where T : IComparable<T>
         {
-            if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
+            private T first_Value;
+            private T second_Value;
+            private T third_Value;
+            public MaximumNumber(T first_Value, T second_Value, T third_Value)
             {
-                return first_Value;
+                this.first_Value = first_Value;
+                this.second_Value = second_Value;
+                this.third_Value = third_Value;
             }
-            else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
+            public static T GetMaximum(T first_Value, T second_Value, T third_Value)
             {
-                return second_Value;
+                if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
+                {
+                    return first_Value;
+                }
+                else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
+                {
+                    return second_Value;
+                }
+                else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
+                {
+                    return third_Value;
+                }
+                else
+                {
+                    throw new Exception("Values are same");
+                }
+
             }
-            else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
-            {
-                return third_Value;
-            }
-            else
-            {
-                throw new Exception("Values are same");
-            }
+
         }
     }
-}
--- FindMaxNumber/FindMaxNumber/Program.cs
+++ FindMaxNumber/FindMaxNumber/Program.cs
@@ -1,14 +1,16 @@
-﻿namespace FindMaxNumber
+﻿
+
+namespace FindMaxNumber
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int output = MaximumNumber.GetMaximum<int>(11, 22, 33);
+            int output = MaximumNumber<int>.GetMaximum(11, 22, 33);
             Console.WriteLine($"max={output}");
-            float output1 = MaximumNumber.GetMaximum<float>(11.5f, 22.5f, 33.5f);
+            float output1 = MaximumNumber<float>.GetMaximum(11.5f, 22.5f, 33.5f);
             Console.WriteLine($"max={output1}");
-            string output2 = MaximumNumber.GetMaximum<string>("abc", "xyz", "pqr");
+            string output2 = MaximumNumber<string>.GetMaximum("abc", "xyz", "pqr");
             Console.WriteLine($"max={output2}");
         }
 
