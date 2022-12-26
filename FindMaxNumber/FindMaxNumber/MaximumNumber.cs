90752d48d05f30e4f6bc92a25f0de934a89f2813
qismat <qismatnisha6243@gmail.com>
2022-12-26 17:20:32 +0530

    [qismat] Add : UC4-ExtendMaxMethod

--- FindMaxNumber/FindMaxNumber/MaximumNumber.cs
+++ FindMaxNumber/FindMaxNumber/MaximumNumber.cs
@@ -6,38 +6,36 @@ using System.Threading.Tasks;
 
 namespace FindMaxNumber
 {
-   
-        public class MaximumNumber<T> where T : IComparable<T>
-        {
-            private T first_Value;
-            private T second_Value;
-            private T third_Value;
-            public MaximumNumber(T first_Value, T second_Value, T third_Value)
-            {
-                this.first_Value = first_Value;
-                this.second_Value = second_Value;
-                this.third_Value = third_Value;
-            }
-            public static T GetMaximum(T first_Value, T second_Value, T third_Value)
-            {
-                if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
-                {
-                    return first_Value;
-                }
-                else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
-                {
-                    return second_Value;
-                }
-                else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
-                {
-                    return third_Value;
-                }
-                else
-                {
-                    throw new Exception("Values are same");
-                }
 
-            }
+    public class MaximumNumber<T> where T : IComparable<T>
+    {
+        public T[] inputArray;
+
+        public MaximumNumber(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public T[] Sort(T[] inputArray)
+        {
+            Array.Sort(inputArray);
+            return inputArray;
+        }
+
+        public T MaxValue(params T[] inputArray)
+        {
+            var sortedValue = Sort(inputArray);
+            return sortedValue[^1];
 
         }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.inputArray);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.inputArray);
+            Console.WriteLine($"Maximum value :{max}");
+        }
     }
+}
--- FindMaxNumber/FindMaxNumber/Program.cs
+++ FindMaxNumber/FindMaxNumber/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace FindMaxNumber
 {
@@ -6,12 +6,9 @@ namespace FindMaxNumber
     {
         static void Main(string[] args)
         {
-            int output = MaximumNumber<int>.GetMaximum(11, 22, 33);
-            Console.WriteLine($"max={output}");
-            float output1 = MaximumNumber<float>.GetMaximum(11.5f, 22.5f, 33.5f);
-            Console.WriteLine($"max={output1}");
-            string output2 = MaximumNumber<string>.GetMaximum("abc", "xyz", "pqr");
-            Console.WriteLine($"max={output2}");
+            int[] array = { 11, 12, 35, 45, 85 };
+            MaximumNumber<int> generic = new MaximumNumber<int>(array);
+            generic.PrintMaxValue();
         }
 
     }
